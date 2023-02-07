1b37daf906b8d8cf03d23e84aeb65f1dc1b1e8fd
uriferra <urielyb@gmail.com>
2023-02-07 11:07:15 -0400

    test 1 react

---
+++ test1/react-test/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clique aqui
+      </button>
+    </div>
+  );
+}
+
+export default App;
